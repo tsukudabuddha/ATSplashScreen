180c6146567f1490b82af26c0ab94aaa30076b19
tsukudabuddha <andrewtsukuda@gmail.com>
2018-05-12 12:31:40 -0700

    Added logo fade out as default example view

--- Example/ATSplashScreen/ViewController.swift
+++ Example/ATSplashScreen/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        view = SplashScreenView(frame: view.frame, imageName: "apple")
     }
 
     override func didReceiveMemoryWarning() {
